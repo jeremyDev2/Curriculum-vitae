81ba6d5f497701360cd3c56a39eb0f853fb1c4d2
warconsular <danil.malaev3@gmail.com>
2023-06-05 13:11:09 +0300

    lit bit redesign

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,5 @@
 import { Box, Container, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/react';
-import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Breadcrumb, BreadcrumbItem } from '@chakra-ui/react';
 
 
@@ -10,36 +9,35 @@ function Navigation() {
 
   return (
     <Container>
-      <Box id='navBox' borderRadius="lg" bg="#323135" p={3} mb={6} align="center" color={colorMode === 'light' ? textColor : ''} >
-        <Breadcrumb>
-
+      <Box id='navBox' borderRadius="lg" bg="#323135" p={4} mb={6}   color={colorMode === 'light' ? textColor : ''} >
+        <Breadcrumb display="flex">
           <BreadcrumbItem>
             <Link href='https://github.com/warconsular' isExternal color="#4078c0" ml={7}>
-              GitHub<ExternalLinkIcon mx="2px" />
+              GitHub
             </Link>
           </BreadcrumbItem>
 
-          <BreadcrumbItem>
+          <BreadcrumbItem display="flex" >
             <Link href='https://www.linkedin.com/in/dan-malaiev-186b81254/' isExternal color="#0e76a8">
-              Linkedin<ExternalLinkIcon mx="2px" />
+              Linkedin
             </Link>
           </BreadcrumbItem>
 
           <BreadcrumbItem>
             <Link href='mailto:daniil.malaev3@gmail.com' isExternal color="#BB001B">
-              Gmail<ExternalLinkIcon mx="2px" />
+              Gmail
             </Link>
           </BreadcrumbItem>
 
           <BreadcrumbItem>
             <Link href='https://www.hackerrank.com/danil_malaev3' isExternal color="#34A853">
-              HackerRank<ExternalLinkIcon mx="2px" />
+              HackerRank
             </Link>
           </BreadcrumbItem>
 
-          <BreadcrumbItem>
+          <BreadcrumbItem >
             <Link href='https://t.me/war_consular' isExternal color="#0088CC">
-              Telegram<ExternalLinkIcon mx="2px" />
+              Telegram
             </Link>
           </BreadcrumbItem>
 
