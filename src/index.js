47e026350b820849025a1fec978d93e867bfed77
warconsular <danil.malaev3@gmail.com>
2023-06-05 11:49:33 +0300

    change root build

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react';
+import {createRoot} from 'react-dom/client'
 
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+createRoot(root).render(
   <React.StrictMode>
     <ChakraProvider >
         <App />
     </ChakraProvider>
   </React.StrictMode>
-);
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
