376db57a33f947c43dedf0c75b2a81c454232972
warconsular <danil.malaev3@gmail.com>
2023-06-05 13:35:11 +0300

    change nav-bar design

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,6 @@
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 import { Box, Container, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { Link } from '@chakra-ui/react';
-import { Breadcrumb, BreadcrumbItem } from '@chakra-ui/react';
-
 
 function Navigation() {
   const { colorMode } = useColorMode();
@@ -9,39 +8,38 @@ function Navigation() {
 
   return (
     <Container>
-      <Box id='navBox' borderRadius="lg" bg="#323135" p={4} mb={6}   color={colorMode === 'light' ? textColor : ''} >
-        <Breadcrumb display="flex">
-          <BreadcrumbItem>
-            <Link href='https://github.com/warconsular' isExternal color="#4078c0" ml={7}>
-              GitHub
-            </Link>
-          </BreadcrumbItem>
+      <Box id='navBox' display="flex" justifyContent="space-between" borderRadius="lg" bg="#323135" p={4} mb={6} color={colorMode === 'light' ? textColor : ''} >
 
-          <BreadcrumbItem  >
-            <Link href='https://www.linkedin.com/in/dan-malaiev-186b81254/' isExternal color="#0e76a8">
-              Linkedin
-            </Link>
-          </BreadcrumbItem>
 
-          <BreadcrumbItem>
-            <Link href='mailto:daniil.malaev3@gmail.com' isExternal color="#BB001B">
-              Gmail
-            </Link>
-          </BreadcrumbItem>
+        <Link href='https://github.com/warconsular' isExternal color="#4078c0">
+          GitHub <ExternalLinkIcon w={5} />
+        </Link>
 
-          <BreadcrumbItem>
-            <Link href='https://www.hackerrank.com/danil_malaev3' isExternal color="#34A853">
-              HackerRank
-            </Link>
-          </BreadcrumbItem>
 
-          <BreadcrumbItem >
-            <Link href='https://t.me/war_consular' isExternal color="#0088CC">
-              Telegram
-            </Link>
-          </BreadcrumbItem>
 
-        </Breadcrumb>
+        <Link href='https://www.linkedin.com/in/dan-malaiev-186b81254/' isExternal color="#0e76a8">
+          Linkedin <ExternalLinkIcon w={5}/>
+        </Link>
+
+
+
+        <Link href='mailto:daniil.malaev3@gmail.com' isExternal color="#BB001B">
+          Gmail <ExternalLinkIcon w={5}/>
+        </Link>
+
+
+
+        <Link href='https://www.hackerrank.com/danil_malaev3' isExternal color="#34A853">
+          HackerRank <ExternalLinkIcon w={5}/>
+        </Link>
+
+
+        <Link href='https://t.me/war_consular' isExternal color="#0088CC">
+          Telegram<ExternalLinkIcon w={5}/> 
+        </Link>
+
+
+
       </Box>
     </Container>
   );
